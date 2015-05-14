065be9302601edb1b73abf5e01e8879a8e8deb86
Anton Hedstrom <antonhedstrom@gmail.com>
2015-05-14 15:13:27 +0200

    Restored settings.js.

--- settings.js
+++ settings.js
@@ -1,26 +1,48 @@
+
 module.exports = {
+  env: processVar('NODE_ENV', 'ENV') || 'DEV',
   app: {
-    port: 3000,
+    port: processVar('PORT') || 3000,
     session: {
-      secret: '<insert-app-secret>',
+      secret: processVar(true, 'SESSION_SECRET'),
       cookieAge: 3600000*24*7 // 7 days
     }
   },
   db: {
-    mysql: {
-      host: '',
-      user: '',
-      password: '',
-      database: '',
-      charset: 'utf8',
-      pbkdf2: {
-        salt: '<insert-password-salt>',
-        iterations: 7000,
-        keylen: 512,
-        encoding: 'hex',
-        usernameField: 'username', // Not for DB, but for form fields
-        passwordField: 'password' // Not for DB, but for form fields
-      }
-    }
+    client: 'mysql',
+    debug: (processVar('ENV') === 'PROD') ? false : true,
+    connection: processVar(true, 'CLEARDB_DATABASE_URL', 'DB_CONN')
   },
+  pbkdf2: {
+    salt: processVar(true, 'SALT'),
+    iterations: 7000,
+    keylen: 512,
+    encoding: 'hex',
+    usernameField: 'username', // Not for DB, but for form fields
+    passwordField: 'password' // Not for DB, but for form fields
+  }
 };
+
+/* Helper function to chech for process enviroment variables */
+function processVar() {
+  var keys = Array.prototype.slice.call(arguments, 0),
+      isRequired = false;
+
+  if ( keys[0] === true ) {
+    isRequired = true;
+    keys = keys.slice(1);
+  }
+  for(var i=0, max=keys.length; i<max; i++) {
+    if ( isDefined(process.env[keys[i]]) )  {
+      return process.env[keys[i]];
+    }
+  }
+  if ( isRequired ) {
+    throw Error('Couldn\'t find required env var: ' + keys.join(' || '));
+  }
+  return false;
+}
+
+function isDefined(val) {
+  return (val !== undefined && val !== null);
+}
